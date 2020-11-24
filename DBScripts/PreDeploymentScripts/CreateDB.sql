ef725fce86e8807d63034990c8930217ba5986c0
abrenessapiens <aisen.brenes@sapiensdev.com>
2020-11-24 07:14:20 -0600

    Create DB Project

---
+++ DBScripts/PreDeploymentScripts/CreateDB.sql
@@ -0,0 +1,6 @@
+----- Pre-Deployment Script Template							
+----------------------------------------------------------------------------------------
+---- This file contains SQL statements that will be executed before the build script.				
+----------------------------------------------------------------------------------------
+
+CREATE DATABASE SmileDirectClubAisenBrenes;
